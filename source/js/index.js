948d5583bc0ad0421a5d3776be613a2f6db7b727
UrePu <darkin31313@gmail.com>
2022-08-09 17:41:03 +0900

    배경화면 변경

--- source/js/index.js
+++ source/js/index.js
@@ -6,7 +6,6 @@ var mHtml = $(".container");
 var page = 1;
 var header = document.getElementsByClassName("container__header")
 
-console.log(header[0]);
 
 mHtml.animate({scrollTop : 0},10);
 
@@ -25,5 +24,37 @@ $(window).on("wheel", function(e) {
     if(page === 1) header[0].className = "container__header";
     var posTop =(page-1) * $(window).height();
     console.log(posTop + " " + page);
-    mHtml.animate({scrollTop : posTop});
+    mHtml.animate({scrollTop : posTop}, 1000);
 })
+
+var section__1 = document.getElementById("container__section__1");
+var head__text = document.getElementsByClassName("head__text");
+var head__sub__text = document.getElementsByClassName("head__sub__text");
+var bgpage = 1;
+
+
+$(document).ready(function(){
+    setInterval(() => {
+        bgpage++;
+        if(bgpage == 4) bgpage = 1;
+        section__1.style.backgroundImage = 'url("../source/img/main_background0'+bgpage+'.jpg")';
+        switch(bgpage){
+            case 1:
+                head__text[0].innerHTML = "The best solar energy";
+                head__text[1].innerHTML = "equipments from Korea"
+                head__sub__text[0].innerHTML = "We provide the world's highest quality solar energy equipments from various brands.<br>Especially from Korea."
+                break;
+            case 2:
+                head__text[0].innerHTML = "Let the sun work for you";
+                head__text[1].innerHTML = "we ship all over the world"
+                head__sub__text[0].innerHTML = "We only carry the most profitable solar energy equipment.<br>　"
+                break;
+            case 3:
+                head__text[0].innerHTML = "We provide the easiest way to buy";
+                head__text[1].innerHTML = "solar power equipment";
+                head__sub__text[0].innerHTML = "WOrder, Pay and Get, Go through only 3 steps.<br>You can have the best solar equipment, including Korean leading brands."
+                break;
+
+        }
+    }, 5000);
+});
